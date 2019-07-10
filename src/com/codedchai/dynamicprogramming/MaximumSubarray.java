8a9d7fe8aad329ba41511460fe8b8775ea684d97
Connor Hasting <guardianx515@gmail.com>
2019-07-09 21:25:33 -0500

    Added some printouts to the subarray problem

--- src/com/codedchai/dynamicprogramming/MaximumSubarray.java
+++ src/com/codedchai/dynamicprogramming/MaximumSubarray.java
@@ -1,4 +1,27 @@
 package com.codedchai.dynamicprogramming;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class MaximumSubarray {
+
+    public static void main(String[] args){
+        int[] array = {1,2,-3,-4,2,7,-2,3};
+
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+
+        System.out.println("The maximum subarray is: " + maximumSubarray.findMaximumSubarray(array));
+    }
+
+    public int findMaximumSubarray(int[] array){
+        int[] cache = new int[array.length + 1];
+        cache[0] = 0;
+
+        for(int i = 1; i < cache.length; i++){
+            cache[i] = Math.max(cache[i - 1] + array[i-1], array[i-1]);
+            System.out.println("The current maximum is: " + cache[i]);
+        }
+
+        return Arrays.stream(cache).max().getAsInt();
+    }
 }
